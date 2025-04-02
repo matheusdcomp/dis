87275624c1bf62bf3583d84cd6ea297cb1c14dc1
Matheus Viana <matheuscviana@ufsj.edu.br>
2025-04-02 14:25:26 -0300

    Pyhton estático

--- pmd/python/classes/tipagemEstatica.py
+++ pmd/python/classes/tipagemEstatica.py
@@ -4,21 +4,21 @@ import Ponto as pt
 
 class Circulo:
 
-    def __init__(self, centro:pt.Ponto, raio:float):
-        self.centro:pt.Ponto = centro
-        self.raio:float = raio
+  def __init__(self, centro:pt.Ponto, raio:float):
+    self.centro:pt.Ponto = centro
+    self.raio:float = raio
 
-    def area(self): float
-        return 3.1415 * self.raio * self.raio
+  def area(self) -> float:
+    return 3.1415 * self.raio * self.raio
     
-    def pertence(self, p): bool
-        return self.centro.distancia(p) <= self.raio
+  def pertence(self, p) -> bool:
+    return self.centro.distancia(p) <= self.raio
 
 p1 = pt.Ponto(4.0, 6.0)
 p2 = pt.Ponto(3.0, 5.0)
 c1 = Circulo( p1 , 5.0) 
 
 if c1.pertence(p2):
-    print("O ponto ", p2, " pertence ao circulo.")
+  print("O ponto ", p2, " pertence ao circulo.")
 else:
-    print("O ponto ", p2, " nao pertence ao circulo.")
+  print("O ponto ", p2, " nao pertence ao circulo.")
