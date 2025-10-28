d7851d7a5d40aa1cd96cf9248a5dbc4b0a721268
Matheus Viana <matheuscviana@ufsj.edu.br>
2025-10-28 10:05:25 -0300

    28/10

--- pmd/java/excecoes/MinhaExcecao.java
+++ pmd/java/excecoes/MinhaExcecao.java
@@ -5,9 +5,9 @@ public class MinhaExcecao extends Exception {
   public MinhaExcecao() {
     super("Ocorreu minha exceção.");
   }
-  /*
-   * public String getMessage() {
-   * return "Ocorreu minha exceção.";
-   * }
-   */
+  /* 
+  public String getMessage() {
+   return "Ocorreu minha exceção.";
+  }
+*/
 }
