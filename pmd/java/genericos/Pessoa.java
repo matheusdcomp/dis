befd5e73e2def44a723caaebc5ff1448af16f124
Matheus Viana <matheuscviana@ufsj.edu.br>
2025-10-30 10:01:41 -0300

    30/10

--- pmd/java/genericos/Pessoa.java
+++ pmd/java/genericos/Pessoa.java
@@ -4,10 +4,12 @@ public class Pessoa implements Comparable<Pessoa> {
 
   protected String nome;
   protected int idade;
+  protected float altura;
 
-  public Pessoa(String n, int i) {
+  public Pessoa(String n, int i, float a) {
     nome = n;
     idade = i;
+    altura = a;
   }
 
   public String getNome() {
@@ -22,8 +24,16 @@ public class Pessoa implements Comparable<Pessoa> {
     return idade;
   }
 
+  public float getAltura() {
+    return altura;
+  }
+
   public void setIdade(int idade) {
     this.idade = idade;
+  }
+
+  public void setAltura(float altura) {
+    this.altura = altura;
   }
 
   public int compareTo(Pessoa p) {
@@ -32,10 +42,10 @@ public class Pessoa implements Comparable<Pessoa> {
 
   public static void main(String[] a) {
     Pessoa[] ps = new Pessoa[3];
-    ps[0] = new Pessoa("JOAO", 20);
-    ps[1] = new Pessoa("MARIA", 23);
-    ps[2] = new Pessoa("JOSE", 35);
-    int maiores = new Teste<Pessoa>().contaMaiores(ps, new Pessoa("LUCAS", 21), 3);
+    ps[0] = new Pessoa("JOAO", 20, 1.70f);
+    ps[1] = new Pessoa("MARIA", 23, 1.60f);
+    ps[2] = new Pessoa("JOSE", 35, 1.69f);
+    int maiores = new Teste<Pessoa>().contaMaiores(ps, new Pessoa("LUCAS", 21, 1.80f), 3);
     System.out.println(maiores);
   }
 
--- pmd/java/genericos/UsaPessoa.java
+++ pmd/java/genericos/UsaPessoa.java
@@ -8,23 +8,69 @@ public class UsaPessoa {
 
   public static void main(String[] args) {
 
-    Pessoa p1 = new Pessoa("MARIA", 28);
-    Pessoa p2 = new Pessoa("ANTONIO", 35);
-    Pessoa p3 = new Pessoa("JOSE", 20);
+    Pessoa p1 = new Pessoa("MARIA", 28, 1.60f);
+    Pessoa p2 = new Pessoa("ANTONIO", 35, 1.70f);
+    Pessoa p3 = new Pessoa("JOSE", 20, 1.90f);
+
+    int x = p1.compareTo(p3);
+    if (x == 0) {
+      System.out.println(p1.getNome() + " e " + p3.getNome() + " têm a mesma idade.");
+    }
+    else if (x > 0) {
+      System.out.println(p1.getNome() + " é mais velho do que " + p3.getNome());
+    }
+    else if (x < 0) {
+      System.out.println(p1.getNome() + " é mais novo do que " + p3.getNome());
+    }
+
 
     ArrayList<Pessoa> pessoas = new ArrayList<>();
     pessoas.add(p1);
     pessoas.add(p2);
     pessoas.add(p3);
 
+
+    //ordenar por idade - usa o compareTo da própria classe Pessoa
+    Collections.sort(pessoas);
+    for (Pessoa p : pessoas)
+      System.out.println(p.getNome() + ": " + p.getIdade());
+      
+    System.out.println(); 
+
+    //ordenar por nome
     Collections.sort(pessoas, new Comparator<Pessoa>() {
       public int compare(Pessoa a, Pessoa b) {
         return a.getNome().compareTo(b.getNome());
       }
     });
+    for (Pessoa p : pessoas)
+      System.out.println(p.getNome());
 
-    for (Pessoa x : pessoas)
-      System.out.println(x.getNome());
+    System.out.println();
+
+    //ordenar por altura
+    Collections.sort(pessoas, new Comparator<Pessoa>() {
+      public int compare(Pessoa a, Pessoa b) {
+        float res = a.getAltura() - b.getAltura();
+        if (res < 0) return -1;
+        if (res > 0) return 1;
+        return 0;
+      }
+    });
+    for (Pessoa p : pessoas)
+      System.out.println(p.getNome() + ": " + p.getAltura());  
+
+
+    //Comparar todos hierarquicamente: idade, nome e altura
+    Comparator<Pessoa> c = Comparator.comparing((Pessoa p) -> p.getIdade())
+          .thenComparing((Pessoa p) -> p.getNome())
+          .thenComparing((Pessoa p) -> p.getAltura());  
+    
+    Collections.sort(pessoas, c);
+    for (Pessoa p : pessoas)
+      System.out.println(p.getNome()); 
+
+
   }
 
 }
