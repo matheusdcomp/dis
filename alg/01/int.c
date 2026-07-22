7953052bc2e669ac13bf59b584003223d3caf782
matheuscviana <matheuscviana@ufsj.edu.br>
2026-07-22 15:53:07 -0300

    20260722

--- alg/01/int.c
+++ alg/01/int.c
@@ -4,5 +4,5 @@ void main() {
     int num1, num2;
     printf("Insira dois numeros: ");
     scanf("%d %d", &num1, &num2);
-    printf("Você digitou: '%d' e '%d'", num1, num2);
+    printf("Você digitou: '%d' e '%d'\n", num1, num2);
 }
