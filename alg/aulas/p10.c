15ffe8c1a134638d461a80d4cb901e06bc43816b
matheuscviana <matheuscviana@ufsj.edu.br>
2026-09-14 14:38:00 -0300

    20260914

--- alg/aulas/p10.c
+++ alg/aulas/p10.c
@@ -1,15 +1,26 @@
 #include<stdio.h>
 
 void main() {
-  int b, e, r = 1;
-  printf("Digite a base e o expoente: ");
-  scanf("%d %d", &b, &e);
-  int x = e;
-  while(x > 0) {
-    r *= b;
-    x--; 
-  }
+  int b=0, e, r = 1;
 
-  printf("%d eleveado a %d eh %d\n", b, e, r);
+  while (b != -1 ) {
+    printf("Digite um número ou -1 para sair: ");
+    scanf("%d", &b);    
+    
+    if (b != -1) {
+     
+      printf("Digite o segundo número: ");
+      scanf("%d", &e); 
+
+      for(int x = e; x > 0; x-- ) 
+        r *= b;      
+      printf("%d eleveado a %d eh %d\n", b, e, r);
+
+      r = 1;
+      for(int x = b; x > 0; x--) 
+        r *= e;   
+      printf("%d eleveado a %d eh %d\n", e, b, r);
+    }
+  }
  
 }
